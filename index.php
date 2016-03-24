a3fb496bc45fc1f571c2c5db27b80653fd62cb31
Alex Hicks <tiin57@gmail.com>
2016-03-23 23:11:42 -0400

    Add index.php
    For listing things

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Miscellaneous Utilities</title>
+    <style>
+    /* Yes, so creative */
+    a {
+        color: blue;
+    }
+    a:visited {
+        color: blue;
+    }
+    </style>
+</head>
+<body>
+    <h3>Hello!</h3>
+    <p>You're here.</p>
+    <ul>
+        <?php
+        $preUrl = dirname($_SERVER["PHP_SELF"]);
+        $files = array_diff(scandir(__DIR__), ["..", "."]);
+        $dirs = [];
+        foreach ($files as $k => $file) {
+            if (is_dir(__DIR__ . "/" . $file)) {
+                $dirs[] = $file;
+            }
+        }
+        foreach ($dirs as $k => $dir):
+            ?>
+            <li><a href="<?=$preUrl?>/<?=$dir?>"><?=$dir?></a></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
